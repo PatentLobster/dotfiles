b2a31677b3b45e9c6092ed84ee981640f0438405
Itzik <37775846+PatentLobster@users.noreply.github.com>
2022-10-08 00:48:22 +0300

    Chezmoi refresh :recycle:
    Untested configuration :poop:

--- clone.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-git clone https://github.com/PatentLobster/dotfiles ~/.dotfiles --recurse-submodules --remote-submodules
-~/.dotfiles/tools/install.sh
--- tools/update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source $DOTFILES/tools/pretty_print.sh
-
-cd $DOTFILES
-git  pull --rebase --progress 
-git  submodule update --progress --remote --recursive 
-brew bundle --file=$DOTFILES/brew/Brewfile
