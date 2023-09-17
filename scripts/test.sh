9598923ab8bafc4a4d7ff0b4ae778055cae67388
PatentLobster <37775846+PatentLobster@users.noreply.github.com>
2023-09-18 00:19:21 +0300

    ✨

--- scripts/test.sh
+++ scripts/test.sh
@@ -119,7 +119,7 @@ run_test() {
   local -r setup_script="$2"
 
   cmd time docker run --rm --init --interactive --user vscode \
-    --env TERM --env COLORTERM --env REMOTE_CONTAINERS \
+    --env TERM --env COLORTERM --env REMOTE_CONTAINERS=true \
     --volume "${dotfiles_root}:/original-dotfiles:ro" \
     "mcr.microsoft.com/devcontainers/base:${os}" \
     bash <<EOF
