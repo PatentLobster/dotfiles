c4ff2d7a34d60a4a76150d53e0bf823f3b6ddee2
Itzik <37775846+PatentLobster@users.noreply.github.com>
2023-09-23 02:11:47 +0000

    ⚡

--- scripts/test.sh
+++ scripts/test.sh
@@ -118,7 +118,7 @@ run_test() {
   local -r os="$1"
   local -r setup_script="$2"
   local -r post_script="$3"
-  cmd time docker run --rm --init --interactive --user vscode \
+  cmd time docker run --rm --init -ti --user vscode \
     --env TERM --env COLORTERM --env REMOTE_CONTAINERS=true \
     --volume "${dotfiles_root}:/original-dotfiles" \
     "mcr.microsoft.com/devcontainers/base:${os}" \
@@ -179,7 +179,7 @@ for variant in "${variants[@]}"; do
       ;;
 
     tmux)
-      ./install.sh  && scripts/vhs/tmux-vhs.sh
+      run_test "${os}" "" "ls ~/.repo &&   ~/.repo/scripts/vhs/tmux-vhs.sh"
       ;;
 
     devcontainer)
