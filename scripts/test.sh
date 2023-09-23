f63e192180a48c30f3db7999c5be4dd6fdb796a4
Itzik <37775846+PatentLobster@users.noreply.github.com>
2023-09-23 01:20:06 +0000

    ⚡

--- scripts/test.sh
+++ scripts/test.sh
@@ -179,7 +179,7 @@ for variant in "${variants[@]}"; do
       ;;
 
     devcontainer)
-      run_test "${os}" "" "ls ~/.repo &&   ~/.repo/scripts/vhs.sh"
+      run_test "${os}" "" "ls ~/.repo &&   ~/.repo/scripts/vhs/tmux-vhs.sh"
       ;;
 
     zsh)
